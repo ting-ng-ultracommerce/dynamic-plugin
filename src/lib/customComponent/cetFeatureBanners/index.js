f0069e977e51792b6516e033d02c307cc040c5c2
Ting Ng <tingng@Unknown.gateway>
2023-02-21 15:32:21 +1100

    Init repo

---
+++ src/lib/customComponent/cetFeatureBanners/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const cetFeatureBanners = (props) => (
+    <div className="feature-banner">
+        here is custom feature banner
+        <div>{JSON.stringify(props)}</div>
+    </div>
+);
+
+export { cetFeatureBanners };
